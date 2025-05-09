a9de0b9295dbc9ec6c4e757e50402fe243aed987
Junior SEVERE <severejunior2017@gmail.com>
2025-05-09 15:59:24 -0700

    final version hello

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello world!</h1>")
+    return render(request, "hello/index.html")
 
 def junior(request):
     return HttpResponse("<h1>Hello Junior!</h1>")
 
 def greet(request, name):
-    return HttpResponse(f"Hello {name.capitalize()}")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
