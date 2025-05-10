aafb523f0eb41f6b7b1162bf883e6e7427583c2f
Junior SEVERE <severejunior2017@gmail.com>
2025-05-10 11:21:56 -0700

    first version form in django

---
+++ django_forms/myapp/forms.py
@@ -0,0 +1,9 @@
+from random import choices
+from django import forms 
+
+class contactForm(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField(label="E-mail")
+    category = forms.ChoiceField(choices=[('question', 'Question'), ('other', 'Other')])
+    subject = forms.CharField(required=False)
+    body = forms.CharField(widget=forms.Textarea)
--- django_forms/myapp/views.py
+++ django_forms/myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .forms import contactForm
 # Create your views here.
+
+def contact(request):
+    
+    form = contactForm()
+    return render(request, 'myapp/form.html', {
+        'form': form 
+    })
