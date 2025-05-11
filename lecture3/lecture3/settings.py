181c05a9200883949ec6b68f89762400e6082133
Junior SEVERE <severejunior2017@gmail.com>
2025-05-11 10:09:43 -0700

    final version tasks

--- lecture3/lecture3/settings.py
+++ lecture3/lecture3/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello',
+    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,8 @@ ROOT_URLCONF = 'lecture3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
