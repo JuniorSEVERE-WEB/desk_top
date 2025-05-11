81404b4f717e02a7873ca1a57c0f3897d1e8e3c6
Junior SEVERE <severejunior2017@gmail.com>
2025-05-11 15:53:01 -0700

    version-taches before-session

--- remixlecture3/mestaches/views.py
+++ remixlecture3/mestaches/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse 
+from django.http import HttpResponsePermanentRedirect
 
 # Create your views here.
 tasks = ["foo", "poo", "goo"]
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
 def index(request):
     return render(request, "mestaches/index.html", {
         "tasks": tasks
     })
     
 def add(request):
-    return render(request, "mestaches/add.html")    
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponsePermanentRedirect(reverse("index"))
+        else:
+            return render(request, "mestaches/add.html",{
+                "form": NewTaskForm()
+            })
+    form = NewTaskForm()
+    return render(request, "mestaches/add.html",{
+        "form": form
+    })    
     
