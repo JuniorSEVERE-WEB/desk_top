f539653e8132c4aae5fa6b1c5014686058c3babc
Junior SEVERE <severejunior2017@gmail.com>
2025-05-18 07:11:15 -0700

    entry-page-final

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -16,10 +16,17 @@ def index(request):
         "entries": util.list_entries()
     })
     
+    
 def entry(request, title):
     html_content = convert_md_to_html(title)  
     if html_content == None:
-        return render(request, "encyclopedia/error.html")
+        return render(request, "encyclopedia/error.html", {
+            "message": "This entry does not exist"
+        })
     else:
-        return render(request, "encyclopedia/entry.html")
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })
 
+ 
