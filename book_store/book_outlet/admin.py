09c6880e572becbe0be9466ba661602dc5cae8c5
Junior SEVERE <severejunior2017@gmail.com>
2025-06-09 18:34:31 -0700

    adding a one to many relation migration

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -4,5 +4,7 @@ from .models import Book
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("authors", "rating",)
+    list_display = ("title", "authors",)
 
 admin.site.register(Book, BookAdmin)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,6 +4,10 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
@@ -11,18 +15,14 @@ class Book(models.Model):
             MinValueValidator(1),  # ✅ Validation numérique
             MaxValueValidator(5)
         ])
-    authors = models.CharField(null=True, max_length=100)
+    authors = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False,db_index=True) 
     
     
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.slug])
-    
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
-        
+
         
     def __str__(self):
         return f"{self.title} ({self.rating})"
