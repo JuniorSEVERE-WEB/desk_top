1ea3f5a1e95a6b4305003de0c01eab9755d3a27d
Junior SEVERE <severejunior2017@gmail.com>
2025-06-14 14:59:30 -0400

    first-commit

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Blog 
 
 # Create your views here.
+def blog_list(request):
+    posts = Blog.objects.all()
+    carousel = Blog.objects.all().order_by("-id")[:4]
+
+
+    return render(request, "list.html",
+                  {
+                      "posts": posts,
+                      "carousel":carousel
+                  })
+
+def blog_detail(request):
+    return render(request, "detail.html")
