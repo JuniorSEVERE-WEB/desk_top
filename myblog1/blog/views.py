c664ce8ac05b614a6d06e15884bb158c41e8aa63
Junior SEVERE <severejunior2017@gmail.com>
2025-06-15 18:28:55 -0400

    blog post paret un peu normal

--- myblog1/blog/views.py
+++ myblog1/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Blog, Comment 
 
 # Create your views here.
 def blog_list(request):
-    return render(request, "list.html")
+    posts = Blog.objects.all()
+    carousel = Blog.objects.all().order_by("-id")[:4]
+    return render(request, "list.html", {
+        "posts": posts,
+        "carousel": carousel
+    })
 
 def blog_detail(request):
     return render(request, "detail.html")
