a65398ad66a92f24ac41913ab3f7133ff9f57e77
Junior SEVERE <severejunior2017@gmail.com>
2025-06-16 16:33:17 -0400

    displaying blog posts on the homepage

--- myblog3/blog/models.py
+++ myblog3/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings 
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    content = models.TextField()
+    image = models.ImageField(upload_to='blog_images/', blank=True, null =True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.title 
+    
+class Comment(models.Model):
+    posts = models.ForeignKey(Blog,
+                              related_name="comments",
+                              on_delete=models.CASCADE)    
+    name = models.CharField(max_length=300)
+    email = models.EmailField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
--- myblog3/myblog3/settings.py
+++ myblog3/myblog3/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -127,5 +127,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Si vous voulez utiliser un dossier "static" à la racine
 ]
