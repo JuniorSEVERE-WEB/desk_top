43d9c212035c7b30c8902e9ed43a5de156a3eb84
Junior SEVERE <severejunior2017@gmail.com>
2025-06-19 21:47:50 -0400

    after models

--- remixmyblog1/remixmyblog1/urls.py
+++ remixmyblog1/remixmyblog1/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
+from django.conf import settings # Pour les fichiers médias
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("blog.urls"))
+    path("", include("blog.urls")),
+    path('ckeditor5/', include('django_ckeditor_5.urls')),
 ]
+
+
+# Ajoutez ces lignes pour servir les fichiers médias en développement
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
