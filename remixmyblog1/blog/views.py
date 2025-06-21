a295919fceb93740b6aaf74e2e3c732e5580ff4e
Junior SEVERE <severejunior2017@gmail.com>
2025-06-20 23:56:46 -0400

    essaie

--- remixmyblog1/blog/views.py
+++ remixmyblog1/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Blog, Comment
 
 # Create your views here.
 def blog_list(request):
-    return render(request, "blog/list.html")
+    posts = Blog.objects.all()
+    
+    return render(request, "blog/list.html",{
+        "posts":posts,
+        
+    })
 
 def posts_detail(request, id):
     return render(request, "blog/detail.html")
