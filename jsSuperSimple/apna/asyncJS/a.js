f3cff75cdac416746830def1d1e2a5c51bf90649
Junior SEVERE <severejunior2017@gmail.com>
2025-07-12 22:46:40 -0400

    commit1

---
+++ jsSuperSimple/apna/asyncJS/a.js
@@ -0,0 +1,12 @@
+console.log("I");
+console.log("eat");
+setTimeout(()=>{
+    console.log("ice cream");
+},3000)
+console.log("with a");
+console.log("spoon");
+
+
+
+//let abscd = ()=>{}
+//function abascd(){}    
