e032756e17e99263a86abef2dffc5ff5a73f7204
Junior SEVERE <severejunior2017@gmail.com>
2025-08-14 16:10:14 -0400

    Create multiple pages using react-router

--- react-course/ecommerce-project/src/App.jsx
+++ react-course/ecommerce-project/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    
-      <HomePage />
-
+    <Routes>
+          <Route path="/" element={ <HomePage />}></Route>
+          <Route path="checkout" element={<div>Test checkout</div>}></Route>
+    </Routes>
+     
   )
 }
-  
+
 export default App
