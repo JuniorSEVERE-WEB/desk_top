1dbb18e3757df5fd58c51b6519d174db3eaf076b
Junior SEVERE <severejunior2017@gmail.com>
2025-08-14 21:57:36 -0400

    install react-router-dom and fix header.css import path

---
+++ ecommerce-react/src/App.jsx
@@ -0,0 +1,19 @@
+import { HomePage } from './pages/HomePage'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Routes>
+      <Route index element= { <HomePage /> }/>
+      
+    </Routes>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
