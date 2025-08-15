34c448fed495cc43fc4f1c16c241bff728ded7af
Junior SEVERE <severejunior2017@gmail.com>
2025-08-15 16:46:00 -0400

    final 5k

---
+++ react-course/exo-lesson5/5k/chatbot-project/src/components/ChatMessage.jsx
@@ -0,0 +1,52 @@
+import RobotProfileImage from '../assets/robot.png';
+import UserProfileImage from '../assets/image.png';
+import './ChatMessage.css';
+import dayjs from 'dayjs';
+
+export function ChatMessage({ message, sender, time }) {
+        // const message = props.message;
+        // const sender = props.sender;
+        // const { message, sender } = props;
+
+        /*
+        if (sender === 'robot') {
+          return (
+            <div>
+              <img src="robot.png" width="50" />
+              {message}
+            </div>
+          );
+        }
+        */
+
+        return (
+          
+          
+          <div className={
+            sender === "user"
+            ? "chat-message-user"
+            :"chat-message-robot"
+          }>
+            {sender === 'robot' && (
+              <img src={RobotProfileImage} className="chat-message-profile" width="50"/>
+            )}
+            <div className="chat-message-text">
+                {message}
+
+                {/* The "time && (" check is optional. I added it just to be safe. */}
+            {time && (
+              <div className='chat-message-time'>
+                {dayjs(time).format('h:mma')}
+              </div>
+            )}
+
+
+            </div>    
+            {sender === 'user' && (
+              <img src={UserProfileImage} className="chat-message-user" width="50"/>
+            )}
+          </div>
+
+          
+        );
+      }
