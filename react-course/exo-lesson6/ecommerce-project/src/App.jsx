025faa46d42556fd1ead3b36543af7728fa57ab6
Junior SEVERE <severejunior2017@gmail.com>
2025-08-17 18:02:04 -0400

    starting 6a

---
+++ react-course/exo-lesson6/ecommerce-project/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <Routes>
+          <Route index element={ <HomePage />} />
+          <Route path="checkout" element={<CheckoutPage />} />
+          <Route path="orders" element={ <OrdersPage /> }/>
+    </Routes>
+     
+  )
+}
+
+export default App
