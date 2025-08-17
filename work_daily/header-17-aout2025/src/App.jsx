c041feb47d9938c6db72f690f1d5850422576160
Junior SEVERE <severejunior2017@gmail.com>
2025-08-17 17:55:44 -0400

    middle time work

---
+++ work_daily/header-17-aout2025/src/App.jsx
@@ -0,0 +1,16 @@
+import { HeaderPage } from './components/HeaderPage'
+import { HomePage } from './pages/HomePage'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <HeaderPage />
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ work_daily/header-17-aout2025/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+
+import './homepage.css'
+
+
+export function HomePage()
+{
+    return(
+        <>
+          
+        </>
+    );
+}
