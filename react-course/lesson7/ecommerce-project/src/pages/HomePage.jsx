b7c14244d08bf6d7421512c92cf2763a0a634727
Junior SEVERE <severejunior2017@gmail.com>
2025-08-20 13:37:42 -0400

    fetch data from the backend

--- react-course/lesson7/ecommerce-project/src/pages/HomePage.jsx
+++ react-course/lesson7/ecommerce-project/src/pages/HomePage.jsx
@@ -2,6 +2,17 @@ import { Header } from "../components/header";
 import "./HomePage.css";
 import { products } from "../../starting-code/data/products";
 export function HomePage() {
+
+  fetch("http://localhost:3000/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
+
+
   return (
     <>
       <title>Ecommerce project</title>
