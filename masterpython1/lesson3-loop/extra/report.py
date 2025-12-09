7e448c45e4ee718199c2a82492d7076fe2a3f3ab
Junior SEVERE <severejunior2017@gmail.com>
2025-12-09 13:52:27 -0500

    begin report.py

---
+++ masterpython1/lesson3-loop/extra/report.py
@@ -0,0 +1,12 @@
+def main():
+    ...
+
+def create_report(spacecraft):
+    return f""" 
+       ===== REPORT ======
+
+       Name: TODO
+       Distance: TODO 
+
+       =====================
+    """    
