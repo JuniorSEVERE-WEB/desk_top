b5391b1230f2b91e041a920e61fcaa124073b69f
Junior SEVERE <severejunior2017@gmail.com>
2025-12-15 21:25:40 -0500

    calculatrice.py

---
+++ masterpython1/lesson4-exceptions/calculatrice.py
@@ -0,0 +1,19 @@
+#1-function main()
+   #1.0-ask if add, mult, div, minus
+   #1.1-enter a value
+   #1.3-give you the table you want
+
+def table_add(n):
+    for i in range(11):
+        return f"{n} + {i} = {n + i}" 
+    
+def table_mult(n):
+    for i in range(11):
+        return f"{n} * {i} = {n * i}"   
+
+def table_minus(n):
+    for i in range(11)
+    return n - i  
+
+def main():
+    ask = ask_operator("Type:  \n 1 for add  \n 2 for mult \n 3 for div  \n 4 f  ")
