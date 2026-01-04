34d0913a5020ff6fdc89156d1cfd4ad663e9c8b1
Junior SEVERE <severejunior2017@gmail.com>
2026-01-04 18:26:51 -0500

    update1

---
+++ masterpython1/unpacking/server.py
@@ -0,0 +1,18 @@
+#Importe la bibliothèque standard de Python pour la communication réseau
+import socket 
+
+#L'adresse IP locale (localhost). Le serveur ne sera accessible que depuis votre propre ordinateur.
+HOST = "127.0.0.1"
+
+#Le port d'écoute. C'est comme le numéro d'appartement sur un immeuble (l'IP étant l'adresse de l'immeuble).
+PORT = 8080 
+##
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM):
+
+
+
+
+
+
+
